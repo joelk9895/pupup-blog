c519060bacfc966cd89610274dd8d0f064576145
Joel K George <joelk9895@gmail.com>
2025-11-11 10:28:13 +0530

    final

--- app/blog/route.ts
+++ app/blog/route.ts
@@ -5,16 +5,22 @@ const posts = [
     slug: "bob-riley-lumi-three-puppies-journeys-to-singapore-with-pupup",
     title: "Bob, Riley & Lumi: Three Puppies' Journeys to Singapore with PupUp",
     date: "2025-04-24T00:00:00.000Z",
+    feature_image:
+      "https://framerusercontent.com/images/SGE2AzITvWcozpyZJYHiVZxO0.png",
   },
   {
     slug: "a-miniature-dachshund-s-journey-from-australia-to-singapore",
     title: "Toby - A Miniature Dachshund's Journey from Australia to Singapore",
     date: "2024-11-03T00:00:00.000Z",
+    feature_image:
+      "https://framerusercontent.com/images/I5VV9nJTDFvuKYv9ZKzuEpg1Guk.webp",
   },
   {
     slug: "mini-dachshunds-the-perfect-pup",
     title: "Mini Dachshunds: The Perfect Pup for Your Family?",
     date: "2024-10-26T00:00:00.000Z",
+    feature_image:
+      "https://framerusercontent.com/images/cjKSazuRPUluto6HcuUG4xso.webp",
   },
 ];
 
